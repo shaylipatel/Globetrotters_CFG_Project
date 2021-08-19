f0d228aa4ac6651ddb3f70eb89cc1bf6e07f081a
NosheenMasud <75260092+NosheenMasud@users.noreply.github.com>
2021-08-19 12:24:36 +0100

    Update functions.py
    added tkinter function

--- functions.py
+++ functions.py
@@ -57,3 +57,55 @@ def play_again():
         print('Goodbye!')
         exit()
 
+        
+
+from tkinter import *
+
+def graphics_box():
+    #key down function
+    def click():
+        entered_text=textentry.get().title().strip()  # this will collect the text from the text box
+        output.delete(0.0, END)
+        try:
+            definition = my_compdictionary[entered_text]
+        except:
+            definition = "sorry there is no such location, please try again"
+        output.insert(END, definition)
+    ##### main:
+    window = Tk()
+    window.title("Where is Ada Lovalace?")
+    window.configure(background="black")
+    #### My Photo
+    photo1 = PhotoImage(file="globe.gif")
+    Label (window, image=photo1,bg="black") .grid(row=0, column=0, sticky=N)
+    #create label
+    Label (window, text="Which location would you like to go to?", bg="black",  fg="white", font="none 12 bold") .grid(row=1, column=0, sticky=N)
+    #create text entry box
+    textentry = Entry(window, width=20, bg="white")
+    textentry.grid(row=2, column=0, sticky=N)
+    #add a submit button
+    Button(window, text="SUBMIT", width=6, command=click) .grid(row=3, column=0, sticky=N)
+    #create another label
+    Label (window, text="\nClue:", bg="black",  fg="white", font="none 12 bold") .grid(row=4, column=0, sticky=N)
+    # create an output text box
+    output = Text(window, width=40, height=6, wrap=WORD, background="white")
+    output.grid(row=5, column=0, columnspan=2, sticky=N)
+    #the dictionary
+    my_compdictionary = { #need to connect dictionary to clues dictionaries, and base it on choice of location
+        'Buckingham Palace': 'It is one of only three surviving city-states in the world',
+        'Tower Of London':'Bukit Timah Nature Reserve holds more species of trees than the entire North American continent',
+        'London Eye': 'The national language is Malay'
+        }
+    #exit label
+    Label (window, text="click to exit", bg="black",  fg="white", font="none 12 bold") .grid(row=6, column=0, sticky=N)
+    #exit function:
+    def close_window():
+        window.destroy()
+        exit()
+    #exit button:
+    Button(window, text="Exit", width=14, command=close_window) .grid(row=7, column=0, sticky=N)
+    #run the main loop
+    return window.mainloop()
+
+graphics_box()
+
