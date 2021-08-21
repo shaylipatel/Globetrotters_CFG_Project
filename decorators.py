2a40db47849c32bebba246d59c1792ddce6cb918
lanamoroney <70061041+lanamoroney@users.noreply.github.com>
2021-08-21 20:10:14 +0100

    Update decorators.py

--- decorators.py
+++ decorators.py
@@ -9,29 +9,33 @@ def text_wrapper(func):
         print("*" * 120)
     return inner_wrapper
 
+
 @text_wrapper
 def instructions():
     print('Welcome to the search for Ada Lovelace. With her vast knowledge of mathematics and her vision of what was possible \nfor the future of machines, she is today known as the first programmer. However Ada has been travelling around the \nworld for many years, from country to country. As a young budding programmer, your job is to find Ada, and see what \nknowledge and wisdom she is willing to impart to you.\n \nDo you think you have what it takes to find the elusive Ada…? \nFollow the trail of clues she has left behind to see if you can outwit the world’s first ever computer programmer. \n')
     print('Instructions: \n1. You will start in London. Use the clues to resolve where Ada travelled to next. Pick the correct city and you \nwill be transported there to continue the search. Pick the wrong city and you will return to London to try again \n– and try harder. \n2. Follow the search from city to city. If your intellect matches that of Ada, you may be lucky enough to meet her \nat the end and win the game.')
-  
+
+# Function for when the user enters London   
 @text_wrapper
 def london_welcome():
     print('Welcome to sunny London! \n…where the sun shines in between the bursts of rain! Follow the clues to figure out which city Ada went to next.')
 
+# Function for when the user enters Singapore
 @text_wrapper
 def singapore_welcome():
     print('Welcome to Singapore! \nThe city where summer resides all year long with breathtaking skyline views at night of buildings lit to the nines!')
 
+# Function for when the user enters San Francisco
 @text_wrapper
 def san_fran_welcome():
     print('Welcome to San Francisco! The city of the golden gate! The home of Silicon Valley! \nWhat gold will you find here and where will this currency take you next?')
 
-
+# Function for when the user enters Cairo
 @text_wrapper
 def cairo_welcome():
     print('Welcome to the delightful city of Cairo! Careful - Don’t lose your sun hat in the hustle and bustle! \nLet’s see what wisdoms and secrets this ancient city of the Pharaohs is willing to impart with you. \nAnd whether it will give you a treasure or a curse.')
 
-
+# Function for when the user enters Delhi
 @text_wrapper
 def delhi_welcome():
     print('Welcome to Delhi! \nThe street markets are lively, the colours vibrant and the food incredible. \nJust watch out for that Dehli Belly… it can’t get in the way of your mission!')
