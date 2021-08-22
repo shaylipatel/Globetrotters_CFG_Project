3b245611079e1e1edfb3863eeb6471a09925a684
NosheenMasud <75260092+NosheenMasud@users.noreply.github.com>
2021-08-22 13:52:30 +0100

    added example of final key

--- config.py
+++ config.py
@@ -2,10 +2,10 @@
 # Please register at OpenWeatherMap.org for an api key to allow the usage of real time temperature accessible via
 # getweather() function located in the functions.py module.
 # Once you have the api key, insert into the string below.
+# The key should look like this once an api key as been inserted:  
+# api_key = f'd26034kkd89olo322qw6refe89b99a61'  (please note this is not a working key)
 
 api_key = f'insert here'
-
-
 
 
 
