826cb40c2ee391ede64c64210325a1de35241616
lanamoroney <70061041+lanamoroney@users.noreply.github.com>
2021-08-22 16:00:44 +0100

    Update test_functions.py

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -4,19 +4,31 @@ import unittest
 from functions import affirmation, welcome
 
 class TestStringMethods(unittest.TestCase):
+    
     def test_affirmation(self, expected=True):
+        # Test the affirmation function
+        
+        # Action
         capturedOutput = io.StringIO()   # Create StringIO object
-        sys.stdout = capturedOutput  # and redirect stdout
-        affirmation()  # call function
-        # check stdout output
+        sys.stdout = capturedOutput  # Rredirect stdout
+        affirmation()  # Call function
+        # Check stdout output
+        
+        # Assert
         self.assertTrue(expected, capturedOutput.getvalue())
 
     def test_welcome(self, expected=True):
+        # Test the welcome function
+        
+        # Action
         capturedOutput = io.StringIO()   # Create StringIO object
-        sys.stdout = capturedOutput  # and redirect stdout
-        welcome()  # call function
-        # check stdout output
+        sys.stdout = capturedOutput  # Redirect stdout
+        welcome()  # Call function
+        # Check stdout output
+        
+        # Assert
         self.assertTrue(expected, capturedOutput.getvalue())
 
+        
 if __name__ == '__main__':
     unittest.main()
