5fb9fa141e5abe03b433b007da29a984262c02bf
Catherine <catherineep4189@gmail.com>
2021-08-22 12:08:20 +0100

    config file added with api key guidance

---
+++ config.py
@@ -0,0 +1,11 @@
+# PREREQUISITE
+# Please register at OpenWeatherMap.org for an api key to allow the usage of real time temperature accessible via
+# getweather() function located in the functions.py module.
+# Once you have the api key, insert into the string below.
+
+api_key = f'insert here'
+
+
+
+
+
